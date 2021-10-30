ac8a502924cc4d2d569dfb92e298f0110dcc3fdc
ahdiatahsan <ahdiatahsanaa@gmail.com>
2021-10-30 17:41:20 +0800

    delete double alert

--- resources/views/layouts/partials/alerts.blade.php
+++ resources/views/layouts/partials/alerts.blade.php
@@ -96,27 +96,6 @@
 </div>
 @endif
 
-@if ($message = session()->get('not_match'))
-<div class="row justify-content-center">
-    <div class="col-12">
-        <div class="alert alert-solid-danger alert-bold fade show kt-margin-t-5 kt-margin-b-25" role="alert">
-            <div class="alert-icon"><i class="fa fa-exclamation-triangle"></i></div>
-            <div class="alert-text">
-                <h4 class="alert-heading">Terjadi kesalahan !</h4>
-                <p class="mb-0">
-                    Password lama tidak sesuai dengan yang Anda masukkan.
-                </p>
-            </div>
-            <div class="alert-close">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true"><i class="la la-close"></i></span>
-                </button>
-            </div>
-        </div>
-    </div>
-</div>
-@endif
-
 @if ($errors->any())
 <div class="row justify-content-center">
     <div class="{{ request()->routeIs('profil*') ? 'col-12' : 'col-lg-10 col-md-10 col-sm-12' }}">
