62805de58c4c6c35d393a4d2ea7f1a15f1e368fa
ahdiatahsan <ahdiatahsanaa@gmail.com>
2022-01-18 19:29:16 +0800

    add about paragraph

--- resources/views/user/tentang.blade.php
+++ resources/views/user/tentang.blade.php
@@ -26,9 +26,8 @@
         <h2 class="title-section">Dinas Komunikasi Dan Informatika Kota Makassar</h2>
         <div class="divider"></div>
 
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-        <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nisi saepe eum ipsa. Tempore dolore itaque est blanditiis libero fugiat, ea nostrum nam at tempora quis, facilis officiis nemo mollitia.</p>
+        <p>Dinas Komunikasi dan Informatika merupakan unsur pelaksana urusan pemerintahan bidang komunikasi dan informatika, urusan pemerintahan bidang persandian, dan urusan pemerintahan bidang statistik yang dipimpin oleh Kepala Dinas yang berkedudukan di bawah dan bertanggung jawab kepada Bupati melalui Sekretaris Daerah.</p>
+        
       </div>
       <div class="col-lg-6 py-3">
         <div class="img-fluid py-3 text-center">
